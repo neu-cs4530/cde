af7cdda9d7e0fca6c21716743b5a6787a265db8b
harry <harrisonceckert@icloud.com>
2025-03-13 14:11:29 -0400

    Added Project schema, model.

--- server/models/project.model.ts
+++ server/models/project.model.ts
@@ -1,3 +1,19 @@
-// TODO 
-import { Model } from 'mongoose';
+import mongoose, { Model } from 'mongoose';
+import projectSchema from './schema/project.schema';
+import { DatabaseProject } from '../types/types';
 
+/**
+ * Mongoose model for the `Project` collection.
+ *
+ * This model is created using the `Project` interface and the `projectSchema`, representing the 
+ * `Project` collection stored in the MongoDB database, and provides an interface for interacting 
+ * with the stored projects.
+ *
+ * @type {Model<DatabaseProject>}
+ */
+const ProjectModel: Model<DatabaseProject> = mongoose.model<DatabaseProject>(
+  'Project',
+  projectSchema,
+);
+
+export default ProjectModel;
--- server/models/schema/project.schema.ts
+++ server/models/schema/project.schema.ts
@@ -1,10 +1,30 @@
 import { Schema } from 'mongoose';
-// TODO
 /**
  * Mongoose schema for the Project collection.
  *
  * This schema defines the structure for storing projects in the database.
  * Each project includes the following fields:
  * - `name`: The name of the project.
- * -
+ * - `creator`: The username of the project creator.
+ * - `collaborators`: An array of references to `User` documents that are project collaborators,
+ *   as well as their role.
+ * - `files`: An array of references to `ProjectFile` documents associated with the project.
  */
+const projectSchema: Schema = new Schema(
+  {
+    name: {
+      type: String
+    },
+    creator: {
+      type: String
+    },
+    collaborators: [
+      userId: { type: Schema.Types.ObjectId, ref: 'User' },
+      role: String
+    ], 
+    files: [{ type: Schema.Types.ObjectId, ref: 'ProjectFile' }],
+  },
+  { collection: 'Project' },
+);
+
+export default projectSchema;
