6e772dff15320ccd4f6b307d2c58ffb4d6ae8855
Ariel Park <arielpark456@gmail.com>
2025-04-09 12:17:55 -0400

    a

--- client/src/components/main/projectCard/index.tsx
+++ client/src/components/main/projectCard/index.tsx
@@ -1,30 +1,36 @@
 import { useNavigate } from 'react-router-dom';
+import * as Icons from 'react-icons/fi';
 import { DatabaseProject } from '../../../types/types';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-/**
- * ProjectCard component displays a single project with all of its associated fields such as its name, creation data, current state, and all collaborators.
- *
- * @param project: The project object to display of type Database Project.
- */
+const TrashIcon = Icons.FiTrash2 as React.FC;
 
-const ProjectCard = ({ project }: { project: DatabaseProject }) => {
+const ProjectCard = ({ project, onDelete }: { project: DatabaseProject; onDelete: () => void }) => {
   const navigate = useNavigate();
 
-  /**
-   * Function to handle the navigation of a project.
-   */
   const handleClick = () => {
     navigate(`/projects/${project._id}`);
   };
 
   return (
-    <div className='card shadow-sm hover-card' onClick={handleClick} style={{ cursor: 'pointer' }}>
+    <div
+      className='card shadow-sm hover-card position-relative'
+      onClick={handleClick}
+      style={{ cursor: 'pointer' }}>
+      <button
+        className='btn btn-sm text-danger position-absolute top-0 end-0 m-2'
+        title='Delete Project'
+        onClick={e => {
+          e.stopPropagation();
+          const confirmed = window.confirm('Are you sure you want to delete this project?');
+          if (confirmed) onDelete();
+        }}>
+        <TrashIcon />
+      </button>
       <div className='card-body project'>
         <div className='project-header'>
           <div className='fw-bold mb-2 project-name'>{project.name}</div>
           <div className='text-muted small mb-1 project-curr-state'>
-            {' '}
             Added by: {project.creator}
           </div>
         </div>
